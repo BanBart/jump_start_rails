39197ce1f6a8dfda1e569111abcb644effca7194
sakyhank <sakyhank@gmail.com>
2015-03-29 15:35:13 +0000

    Second and last commit for this short repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :posts
+
+  get 'pages/index'
+
+  %w[about contact cv].each do |page|
+    get page, controller: 'pages', action: page
+  end
+  
+  root :to => 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/models/post_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "post values should not be empty" do
+    post = Post.new
+    assert post.invalid?
+    assert post.errors[:title].any?
+    assert post.errors[:blurb].any?
+    assert post.errors[:body].any?
+  end
+  
+  test "blurb should be a string" do
+    post = Post.new(title: posts(:one).title,
+            blurb: posts(:one).blurb,
+            body: "Body? Whose?")
+    
+    assert post.blurb.kind_of? String
+  end
+end
